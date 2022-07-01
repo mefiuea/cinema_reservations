cfa63a961524a93360a25cea98d381383a729f12
Maciej Koczot <mefiuea@gmail.com>
2022-07-01 21:18:23 +0200

    add new created app to installed apps in settings

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3-rd party
+
+    # local
+    'reservations_app.apps.ReservationsAppConfig',
 ]
 
 MIDDLEWARE = [
