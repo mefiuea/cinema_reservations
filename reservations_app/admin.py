a918ba194871e94288c2089e9a674f6524e17466
Maciej Koczot <mefiuea@gmail.com>
2022-07-02 18:52:51 +0200

    create models, upgrade admin panel

--- reservations_app/admin.py
+++ reservations_app/admin.py
@@ -1,13 +1,33 @@
 from django.contrib import admin
 
-from .models import Author, Movie
+from .models import AuthorModel, MovieModel, CinemaHallModel, RepertoireModel, SeatModel, ReservationModel
 
 
-@admin.register(Author)
+@admin.register(AuthorModel)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name')
 
 
-@admin.register(Movie)
+@admin.register(MovieModel)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'duration')
+
+
+@admin.register(CinemaHallModel)
+class CinemaHallAdmin(admin.ModelAdmin):
+    list_display = ('name', 'size')
+
+
+@admin.register(RepertoireModel)
+class RepertoireAdmin(admin.ModelAdmin):
+    list_display = ('movie', 'cinema_hall', 'screening_date', 'screening_time')
+
+
+@admin.register(SeatModel)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('position',)
+
+
+@admin.register(ReservationModel)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ('buyer', 'repertoire', 'reservation_number')
--- reservations_app/models.py
+++ reservations_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator, MaxLengthValidator
 
 
-class Author(models.Model):
+class AuthorModel(models.Model):
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
 
@@ -11,10 +11,10 @@ class Author(models.Model):
         return f'{self.first_name} {self.last_name}'
 
 
-class Movie(models.Model):
+class MovieModel(models.Model):
     title = models.CharField(max_length=50, blank=False, unique=True)
     release_date = models.IntegerField(blank=False, null=False, default=2000, validators=[MinValueValidator(1800)])
-    author = models.ForeignKey('Author', on_delete=models.PROTECT)
+    author = models.ForeignKey('AuthorModel', on_delete=models.PROTECT)
     duration = models.DurationField(blank=False)
     age_restrictions = models.IntegerField(null=True, blank=True,
                                            validators=[MinValueValidator(0), MaxValueValidator(99)])
@@ -22,3 +22,40 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class CinemaHallModel(models.Model):
+    name = models.CharField(max_length=50, blank=False, unique=True)
+    size = models.IntegerField(blank=False, null=True, validators=[MinValueValidator(0), MaxValueValidator(5000)])
+
+    def __str__(self):
+        return self.name
+
+
+class RepertoireModel(models.Model):
+    movie = models.ForeignKey('MovieModel', on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=5, decimal_places=2,
+                                validators=[MinValueValidator(0), MaxValueValidator(1000)], default=30.00)
+    screening_date = models.DateField()
+    screening_time = models.TimeField()
+    cinema_hall = models.ForeignKey('CinemaHallModel', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'Repertoire:{self.id}, {self.movie}, {self.screening_date}, {self.screening_time}'
+
+
+class SeatModel(models.Model):
+    position = models.CharField(max_length=4)
+
+    def __str__(self):
+        return self.position
+
+
+class ReservationModel(models.Model):
+    buyer = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    repertoire = models.ForeignKey('RepertoireModel', on_delete=models.PROTECT)
+    booked_seats = models.ManyToManyField('SeatModel', related_name='reservation_seat')
+    reservation_number = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Reservation: {self.id}, {self.buyer}, {self.repertoire}, {self.booked_seats}'
