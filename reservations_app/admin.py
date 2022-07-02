4682c1dad2904ea17a718bac4843c42339ac324d
Maciej Koczot <mefiuea@gmail.com>
2022-07-02 17:48:17 +0200

    Create models Author and Movie. Change timezone in settings

--- reservations_app/admin.py
+++ reservations_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Movie
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'duration')
--- reservations_app/models.py
+++ reservations_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, MaxValueValidator, MaxLengthValidator
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50, null=False, blank=False)
+    last_name = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50, blank=False, unique=True)
+    release_date = models.IntegerField(blank=False, null=False, default=2000, validators=[MinValueValidator(1800)])
+    author = models.ForeignKey('Author', on_delete=models.PROTECT)
+    duration = models.DurationField(blank=False)
+    age_restrictions = models.IntegerField(null=True, blank=True,
+                                           validators=[MinValueValidator(0), MaxValueValidator(99)])
+    description = models.TextField(blank=True, null=True, validators=[MaxLengthValidator(2000)])
+
+    def __str__(self):
+        return self.title
