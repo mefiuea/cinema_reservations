89a15f8bf43e4cc5db865d54d2e3d14d04d238fe
Maciej Koczot <mefiuea@gmail.com>
2022-07-04 14:49:32 +0200

    upgrade views in reservations_app

--- reservations_app/views.py
+++ reservations_app/views.py
@@ -1,3 +1,4 @@
+import operator
 from datetime import date
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
@@ -15,26 +16,30 @@ def repertoire_view(request):
         pass
 
     if request.method == 'GET':
+        movies_list = []
         calendar_error = ''
         date_selected = request.GET.get('date')
         if date_selected is None:
             # view today's repertoire
             date_selected = date.today().strftime('%Y-%m-%d')
-            print('TODAY: ', date_selected, flush=True)
-            repertoire = RepertoireModel.objects.filter(screening_date=date.today())
-        else:
-            # view selected repertoire
-            # if wrong or no date selected - validation
-            try:
-                repertoire = RepertoireModel.objects.filter(screening_date=date_selected)
-            except ValidationError as e:
-                repertoire = RepertoireModel.objects.filter(screening_date=date.today())
-                calendar_error = str(e)[5:71].capitalize()
-                print('ERROR: ', calendar_error, type(calendar_error), flush=True)
-        print('REPERTOIRE: ', repertoire, flush=True)
+        # view selected repertoire
+        # if wrong or no date selected - validation
+        try:
+            # get repertoire for a given day with no duplication of titles
+            repertoire = RepertoireModel.objects.filter(screening_date=date_selected).distinct('movie')
+            # to sort alphabetically
+            # repertoire = RepertoireModel.objects.filter(id__in=repertoire).order_by('movie__title')
+            for movie in repertoire:
+                # get list of movies with the same title for a given day
+                movies_by_title = RepertoireModel.objects.filter(movie__title=movie.movie.title, screening_date=date_selected)
+                movies_list.append(movies_by_title)
+            print('MOVIES LIST: ', movies_list, flush=True)
+        except ValidationError as e:
+            repertoire = RepertoireModel.objects.filter(screening_date=date.today()).order_by('screening_time')
+            calendar_error = str(e)[5:71].capitalize()
         context = {
-            'repertoire': repertoire,
             'date_selected': date_selected,
             'calendar_error': calendar_error,
+            'movies_list': movies_list,
         }
         return render(request, 'reservations_app/repertoire.html', context=context)
