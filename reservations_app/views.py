a1c20fcf7bb18c68597ce6ab1f2c4e1b4021b667
Maciej Koczot <mefiuea@gmail.com>
2022-07-04 21:28:44 +0200

    upgrade views to display repertoires by sorting and date

--- reservations_app/views.py
+++ reservations_app/views.py
@@ -44,6 +44,25 @@ def repertoire_view(request):
 
 def get_repertoire_by_selected_sorting(request):
     selected_sorting = request.GET.get('sorting_by')
+    selected_date = request.GET.get('date')
+    if selected_date is None or selected_date == '':
+        # view today's repertoire
+        selected_date = date.today().strftime('%Y-%m-%d')
     print('SELECTED SORTING JAVA SCRIPT: ', selected_sorting, flush=True)
+    print('SELECTED DATE JAVA SCRIPT: ', selected_date, flush=True)
+    if selected_sorting == 'screening_time':
+        order_by = 'screening_time'
+    elif selected_sorting == 'alphabetically':
+        order_by = 'movie__title'
+    elif selected_sorting == 'duration':
+        order_by = 'movie__duration'
+    else:
+        order_by = 'screening_time'
+    repertoires = RepertoireModel.objects.filter(screening_date=selected_date).order_by(order_by)
+    print('REPERTOIRES JAVA SCRIPT: ', repertoires, flush=True)
 
-    return HttpResponse('ok')
+    context = {
+        'repertoires': repertoires,
+    }
+
+    return render(request, 'reservations_app/repertoires_list_by_query_string.html', context=context)
--- static/scripts/sorting_repertoire.js
+++ static/scripts/sorting_repertoire.js
@@ -3,17 +3,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     function checked_radio_button() {
         let marked_radio_button = document.querySelector('input[name="sorting"]:checked').value;
+        let date_from_calendar = document.querySelector('input[name="date"]').value;
+        let params_list = [marked_radio_button, date_from_calendar]
         console.log(marked_radio_button)
+        console.log(date_from_calendar)
+        console.log(params_list)
         let params = new URLSearchParams();
         params.append("sorting_by", marked_radio_button)
+        params.append("date", date_from_calendar)
+        console.log(params)
         let address = 'get_repertoire_by_selected_sorting/?' + params.toString();
         console.log(address)
 
         let vf = fetch(address).then(function (response) {
             return response.text();
         }).then(function (data) {
-            console.log('TUTAJ')
-            // return document.getElementById('ins-pag').innerHTML = data;
+            return document.getElementById('repertoires').innerHTML = data;
         })
     }
 
