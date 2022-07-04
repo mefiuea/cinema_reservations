996064d2344b6d1d52d1a817e126d154ed5da4f9
Maciej Koczot <mefiuea@gmail.com>
2022-07-04 18:20:35 +0200

    change display of main repertoire list

--- reservations_app/views.py
+++ reservations_app/views.py
@@ -22,22 +22,19 @@ def repertoire_view(request):
         if date_selected is None or date_selected == '':
             # view today's repertoire
             date_selected = date.today().strftime('%Y-%m-%d')
-        # get repertoire for a given day with no duplication of titles
-        repertoire = RepertoireModel.objects.filter(screening_date=date_selected).distinct('movie')
+        # get repertoire for a given day
+        repertoires = RepertoireModel.objects.filter(screening_date=date_selected).order_by('screening_time')
         # to sort alphabetically
         # repertoire = RepertoireModel.objects.filter(id__in=repertoire).order_by('movie__title')
         # to sort by price
         # repertoire = RepertoireModel.objects.filter(id__in=repertoire).order_by('price')
-        for movie in repertoire:
-            # get list of movies with the same title for a given day
-            movies_by_title = RepertoireModel.objects.filter(movie__title=movie.movie.title,
-                                                             screening_date=date_selected)
-            movies_list.append(movies_by_title)
-        print('MOVIES LIST: ', movies_list, flush=True)
+
+        for repertoire in repertoires:
+            print('SCREENING TIME: ', repertoire.screening_time, type(repertoire.screening_time), flush=True)
 
         context = {
             'date_selected': date_selected,
             'calendar_error': calendar_error,
-            'movies_list': movies_list,
+            'repertoires': repertoires,
         }
         return render(request, 'reservations_app/repertoire.html', context=context)
