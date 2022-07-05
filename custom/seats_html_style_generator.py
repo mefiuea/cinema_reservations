30b4ebc465f00905723e8efffa5c274244d4c33c
Maciej Koczot <mefiuea@gmail.com>
2022-07-05 19:10:06 +0200

    upgrade styles and template seats

--- custom/seats_html_style_generator.py
+++ custom/seats_html_style_generator.py
@@ -1,3 +1,4 @@
+"""script used to generate cinema seats"""
 letters_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
 for l in letters_list:
@@ -9,12 +10,12 @@ for l in letters_list:
                 <div class="box-available p{l.lower()}6">{l}4</div>
                 <div class="box-available p{l.lower()}7">{l}5</div>
                 <div class="box-available p{l.lower()}8">{l}6</div>
-                <div class="box-unavailable p{l.lower()}9">{l}7</div>
+                <div class="box-available p{l.lower()}9">{l}7</div>
                 <div class="box-available p{l.lower()}10">{l}8</div>
                 <div class="box-available p{l.lower()}11">{l}9</div>
                 <div class="box-available p{l.lower()}12">{l}10</div>
                 <div class="box-none p{l.lower()}13">None</div>
-                <div class="box-unavailable p{l.lower()}14">{l}11</div>
+                <div class="box-available p{l.lower()}14">{l}11</div>
                 <div class="box-available p{l.lower()}15">{l}12</div>
                 <div class="box-description p{l.lower()}16">{l}</div>''')
 
@@ -22,7 +23,9 @@ for idx, l in enumerate(letters_list):
     for i in range(1, 17):
         print(f'''.p{l.lower()}{i} |
         grid-column-start: {i};
-        grid-column-end: {1+i};
-        grid-row-start: {idx+1};
-        grid-row-end: {idx+2};
+        grid-column-end: {1 + i};
+        grid-row-start: {idx + 1};
+        grid-row-end: {idx + 2};
     *''')
+
+print('LEN: ', len(letters_list))
