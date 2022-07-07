a6738b767aa7e056990af3b6fc4b351a975763ea
Maciej Koczot <mefiuea@gmail.com>
2022-07-07 11:45:01 +0200

    add comment in custom function

--- custom/get_booked_seats.py
+++ custom/get_booked_seats.py
@@ -1,4 +1,6 @@
 def get_booked_seats(reservation_instance):
+    """Function used to check reserved seats.
+    Returns: list"""
     booked_seats_list = []
     for reservation in reservation_instance:
         for seat in reservation.booked_seats.all():
